1d7651855e1b199333c71f8c79b90ad407491389
Isaac Brist <isaacbrist@gmail.com>
2022-07-23 11:20:03 -0500

    Added some basic styling to the details.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,51 +1,54 @@
-// import React, { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import './MovieList.css'
-// import { experimentalStyled as styled } from '@mui/material/styles';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import Typography from '@material-ui/core/Typography';
-// import Button from '@material-ui/core/Button';
-// import Paper from '@material-ui/core/Paper';
-// import Grid from '@material-ui/core/Grid';
-// import Card from '@material-ui/core/Card';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {useHistory} from 'react-router-dom'
+import { experimentalStyled as styled } from '@mui/material/styles';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
 
-// function Details() {
+function Details() {
+console.log('In Details')
+    const dispatch = useDispatch();
+    const details = useSelector(store => store.details);
+    const genres=details.genres
+    const history = useHistory();
+    const handleClick=(id)=>{
+        
+        console.log('Go back to the list.')
+history.push('/')
+    }
+    return (
+        <main>
+            <h1>Details</h1>
+            <section className="detail">
+                <div
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                container spacing={4}
+                item sm={3}>
+                    <Paper>
+                        <Card >
+                            <Typography gutterBottom variant="h5" component="h3">{details.title}</Typography>
+                            <img src={details.poster} alt={details.title}/>
+                            <Typography gutterBottom variant="h6" component="h6">{details.description} </Typography>
+                            <Typography gutterBottom variant="h6" component="h6">Genres:</Typography>
+                                {genres?.map(name => {
+                                    return (
+                                        <Typography variant="body1" gutterBottom component="div" key={name}>{name}</Typography>
+                                            )
+                                        })}    
+                            <Button size="large" variant="contained" onClick={handleClick}>Return to List</Button>
+                        </Card>
+                    </Paper>              
+                </div>
+            </section>
+        </main>
 
-//     const dispatch = useDispatch();
-//     const movies = useSelector(store => store.movies);
+    );
+}
 
-//     useEffect(() => {
-//         dispatch({ type: 'FETCH_MOVIES' });
-//     }, []);
-
-//     const handleClick=(id)=>{
-//         console.log('You clicked this movie!', id)
-//     }
-//     return (
-//         <main>
-//             <h1>MovieList</h1>
-//             <section className="movies">
-//                 <Grid container spacing={4}>
-//                 {movies.map(movie => {
-//                     return (
-//                         <Grid item sm={3}>
-//                             <Paper>
-//                                 <Card key={movie.id} onClick={() => handleClick(movie.id)}>
-//                                     <CardActionArea>
-//                                     <Typography gutterBottom variant="h5" component="h3">{movie.title}</Typography>
-//                                     <img src={movie.poster} alt={movie.title}/>
-//                                     </CardActionArea>
-//                                 </Card>
-//                             </Paper>
-//                         </Grid>
-//                     );
-//                 })}
-//                 </Grid>
-
-//             </section>
-//         </main>
-
-//     );
-// }
-
-// export default Details;
+export default Details;
--- src/index.js
+++ src/index.js
@@ -2,93 +2,94 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 // Provider allows us to use redux within our react app
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
+import {takeEvery, put} from 'redux-saga/effects';
 import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('FETCH_MOVIES', fetchAllMovies);
-    yield takeEvery('FETCH_DETAILS', fetchDetails)
+  yield takeEvery('FETCH_MOVIES', fetchAllMovies);
+  yield takeEvery('FETCH_DETAILS', fetchDetails);
 }
 
 function* fetchAllMovies() {
-    // get all movies from the DB
-    try {
-        const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
-        yield put({ type: 'SET_MOVIES', payload: movies.data });
-
-    } catch {
-        console.log('get all error');
-    }
-        
+  // get all movies from the DB
+  try {
+    const movies = yield axios.get('/api/movie');
+    console.log('get all:', movies.data);
+    yield put({type: 'SET_MOVIES', payload: movies.data});
+  } catch {
+    console.log('get all error');
+  }
 }
 //get all details of one movie
 function* fetchDetails(action) {
-try{
-    const detailsResponse= yield axios.get(`/api/movie/${action.payload}`)
-    console.log('In fetchDetails, this is detailsResponse.data', detailsResponse.data)
-    yield put({type: 'SEND_DETAILS', payload: detailsResponse.data}) //might need [0] after  data
-} catch(error) {
-    console.log('error in fetchDetails', error)
-}
+  try {
+    const detailsResponse = yield axios.get(`/api/movie/${action.payload}`);
+    console.log(
+      'In fetchDetails, this is detailsResponse.data',
+      detailsResponse.data
+    );
+    yield put({type: 'SEND_DETAILS', payload: detailsResponse.data[0]}); //might need [0] after  data
+  } catch (error) {
+    console.log('error in fetchDetails', error);
+  }
 }
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_MOVIES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_GENRES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 //Used to store the details of the specified movie
-const details = (state = [], action) => {
-    switch (action.type) {
-        case 'SEND_DETAILS':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+const details = (state = {}, action) => {
+  switch (action.type) {
+    case 'SEND_DETAILS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        movies,
-        genres,
-        details,
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    movies,
+    genres,
+    details,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={storeInstance}>
-        <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
